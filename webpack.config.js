671cbec8b88793fa55b19c6e5dcf7a6c8f243d88
Coyeah <Coyeah_chen@outlook.com>
2018-08-04 11:46:41 +0800

    Updated:A new version of the starter.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 // webpack.config.js
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: __dirname + '/src/index.js',
   output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
-  devtool: 'eval-source-map',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './',  // 本地服务器所加载的页面所在的目录
+    historyApiFallback: true,  // 不跳转
+    inline: true,  // 实时刷新
+    hot: true
+  },
   module: {
     rules: [
       {
@@ -19,7 +28,28 @@ module.exports = {
       },{
         test: /\.scss$/,
         loader: 'style-loader!css-loader!sass-loader'
+      },{
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            query: {
+              limit: 20000,
+              name: 'img/[name]-[hash:5].[ext]'
+            }
+          }, {
+            loader: 'image-webpack-loader'
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin('版权所有，翻版必究'),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  // externals: {
+  //   'react': 'window.React'
+  // }
 }
