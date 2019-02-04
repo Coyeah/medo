6f82c2cd6a7f0ab4bbda512a9b4d05b90ec6216b
Coyeah <Coyeah_chen@outlook.com>
2019-02-04 17:22:00 +0800

    Updated: Readme & introduce

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# friday-project
+# Medo - A memo for Todo
+
+一个在入门 React 的时候练手的项目—— Todo List。再再再一次重写。
+
+## What's different from before?
+
+> 当你不知道是否应该用 redux 的时候，就是不需要用的时候。
+
+在之前开发的 Todo List 中，为了刻意联系，因此尝试在不断的在项目中加各种，如 redux、antd 等。很赘余，很繁重。也没见的多好看。
+
+现在，化繁为简。只用简单的模块化和 react，再加些 antd 的样式作佐料。
+
+Github项目[传送门](https://github.com/Coyeah/medo)
+
+## About CLI
+
+### friday-cli
+
+Create React project with build configuraltion. Develop it now.
+
+If something doesn’t work, please [file an issue](https://github.com/Coyeah/friday-cli/issues).
+
+### friday-project
 
 A React project with build configuraltion. Develop it now.
 
--- src/components/Fixed/index.js
+++ src/components/Fixed/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Drawer, Row, Col} from 'antd';
+import {Drawer, Divider, Row, Col} from 'antd';
 import styles from './index.less';
 
 import logo from '../../assets/logo.svg';
@@ -7,10 +7,21 @@ import logo from '../../assets/logo.svg';
 const InfoContent = () => (
   <div>
     <Row>
-      <Col span={4} >
+      <Col offset={10} span={4} >
         <img src={logo} style={{width: '100%'}}/>
       </Col>
     </Row>
+    <Divider>关于作者</Divider>
+    <p>Coyeah （奇怪的英文名字）</p>
+    <p>大四软工，读着某个不知名的三本，面相前端开发。</p>
+    <p>美剧 & 旅行，想跑去外面多看看。</p>
+    <p>有着很多很多想做的事情，好奇、幻想、爱折腾。</p>
+    <p><b>wubba lubba dub dub</b></p>
+    <Divider>关于项目</Divider>
+    <p>一个在入门 React 的时候练手的项目—— Todo List。再再再一次重写。</p>
+    <p>在之前开发的 Todo List 中，为了刻意联系，因此尝试在不断的在项目中加各种，如 redux、antd 等。很赘余，很繁重。也没见的多好看。</p>
+    <p>现在，化繁为简。只用简单的模块化和 react，再加些 antd 的样式作佐料。</p>
+    <p>Github项目<a href="https://github.com/Coyeah/medo">传送门</a></p>
   </div>
 );
 
@@ -43,7 +54,6 @@ export default class Fixed extends React.PureComponent {
             closable={false}
             onClose={this.onClose}
             placement="right"
-            title="关于作者"
             visible={this.state.visible}
             width={640}
         >
